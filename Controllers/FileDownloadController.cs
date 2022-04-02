ec1379494b1820f5f66e97d4130e79634d9b19ad
Prikhod322 <vovkaprikhod@gmail.com>
2022-04-03 00:35:10 +0300

    Added logging into BD

---
+++ Controllers/FileDownloadController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using WebApplication1;
+
+namespace WebApplication5.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FileDownloadController : ControllerBase
+    {
+        
+        [HttpGet]
+        public StatusCodeResult DownloadFile()
+        {
+            using (SqlConnection connection = new SqlConnection($@"Data Source=tcp:34.88.245.137;Initial Catalog=Logging;User Id=user;Password=mppbl4APe4agoH9P"))
+            {
+                connection.Open();
+
+                string IP = Dns.GetHostEntry(Dns.GetHostName()).AddressList[1].ToString();
+
+                string stringCommand = @$"INSERT INTO [FileDownload]([IP],[Date]) VALUES" +
+                @$"('{IP}','{DateTime.Now} {DateTime.Now.Millisecond}');";
+
+                SqlCommand command = new SqlCommand(stringCommand, connection);
+                command.ExecuteNonQuery();
+
+
+                connection.Close();
+            }
+
+            return StatusCode(200);
+        }
+    }
+}
